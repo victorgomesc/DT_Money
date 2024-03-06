5d540e3cac4e075eefd0a1284117fca3619036ac
victor <victorgomesdacosta18@gmail.com>
2024-03-05 21:32:03 -0300

    DT_Money

---
+++ src/components/NewTransactionModal/index.tsx
@@ -0,0 +1,99 @@
+import * as Dialog from '@radix-ui/react-dialog';
+import { Overlay, Content, CloseButton, TransactionType, TransactionTypeButton } from './styles';
+import { X, ArrowCircleUp, ArrowCircleDown } from 'phosphor-react'
+import * as z from 'zod';
+import { Controller, useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod/dist/zod.js';
+import { useContextSelector } from "use-context-selector";
+import { TransactionsContext } from '../../contexts/TransactionsContext';
+
+const newTransactionFormSchema = z.object({
+    description: z.string(),
+    price: z.number(),
+    category: z.string(),
+    type: z.enum(['income', 'outcome']),
+})
+
+type NewTransactionFormInputs = z.infer<typeof newTransactionFormSchema>;
+
+
+export function NewTransactionModal() {
+    const createTransaction = useContextSelector(TransactionsContext, (context) => {
+        return context.createTransaction;
+    })
+    const { control, register, handleSubmit, formState: { isSubmitting }, reset, } = useForm<NewTransactionFormInputs>({
+        resolver: zodResolver(newTransactionFormSchema),
+        defaultValues: {type: 'income'}
+    })
+
+    async function handleCreateNewTransaction(data: NewTransactionFormInputs) {
+        const { description, price, category, type } = data;
+
+        await createTransaction({
+            description,
+            price,
+            category,
+            type,
+        })
+
+        reset()
+    }
+
+    return(
+        <Dialog.Portal>
+            <Overlay />
+
+            <Content>
+                <Dialog.Title>Nova transação</Dialog.Title>
+                <CloseButton>
+                    <X size={24}/>
+                </CloseButton>
+                    <form onSubmit={handleSubmit(handleCreateNewTransaction)}>
+                        <input 
+                        type='text' 
+                        placeholder='Descrição' 
+                        required 
+                        {...register('description')}
+                        />
+
+                        <input 
+                        type='number' 
+                        placeholder='Preço' 
+                        required 
+                        {...register('price', {valueAsNumber: true})}
+                        />
+
+                        <input 
+                        type='text' 
+                        placeholder='Categoria' 
+                        required 
+                        {...register('category')}
+                        />
+
+                        <Controller 
+                        control={control} 
+                        name="type" 
+                        render={({ field }) => {
+                            return(
+                            <TransactionType onValueChange={field.onChange} value={field.value}>
+                            <TransactionTypeButton variant='income' value='income'>
+                                <ArrowCircleUp size={24}/>
+                                Entrada
+                            </TransactionTypeButton>
+
+                            <TransactionTypeButton variant='outcome' value='outcome'>
+                                <ArrowCircleDown size={24}/>
+                                Saida
+                            </TransactionTypeButton>
+                            </TransactionType>
+                            )
+                        }} />                
+
+                        <button type='submit' disabled={isSubmitting}>
+                            Cadastrar
+                        </button>
+                    </form>
+            </Content>
+        </Dialog.Portal>
+    )
+}
---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,36 @@
+import { SummaryContainer, SummaryCard } from "./styles";
+import { ArrowCircleUp, ArrowCircleDown, CurrencyDollar } from 'phosphor-react'
+import { priceFormatter } from "../../utils/formatter";
+import { useSumary } from "../../hooks/useSummary";
+
+export function Summary() {
+    const summary = useSumary();
+
+    return(
+        <SummaryContainer>
+            <SummaryCard>
+                <header>
+                    <span>Entradas</span>
+                    <ArrowCircleUp size={32} color="#00b37e"/>
+                </header>
+                <strong>{ priceFormatter.format(summary.income) }</strong>
+            </SummaryCard>
+
+            <SummaryCard>
+                <header>
+                    <span>Saidas</span>
+                    <ArrowCircleDown size={32} color="#f75a68"/>
+                </header>
+                <strong>{ priceFormatter.format(summary.outcome) }</strong>
+            </SummaryCard>
+
+            <SummaryCard variant="green">
+                <header>
+                    <span>Total</span>
+                    <CurrencyDollar size={32} color="#fff"/>
+                </header>
+                <strong>{ priceFormatter.format(summary.total) }</strong>
+            </SummaryCard>
+        </SummaryContainer>
+    );
+}
